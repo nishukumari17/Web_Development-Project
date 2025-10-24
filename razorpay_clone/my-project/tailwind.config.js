0ee37ee43a24cd5657d82439174fbb286be29cab
nishukumari17 <nishuavni17@gmail.com>
2025-10-24 19:29:58 +0530

    Working in razorpay clone. Added nav bar

--- razorpay_clone/my-project/tailwind.config.js
+++ razorpay_clone/my-project/tailwind.config.js
@@ -1,13 +1,27 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  // CRUCIAL: This array tells Tailwind to scan these files for classes.
+export default {
   content: [
-    "./index.html",             // Scans your root HTML file
-    "./src/**/*.{js,ts,jsx,tsx}", // Scans all files in src/
+    "./index.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mullish: ["Mulish", "sans-serif"],
+      },
+      colors: {
+        deepBlue: "#02042a",
+        lightBlue: "#2b84ea",
+        lightBlue300: "#4b94ed",
+        lightBlue500: "#0b72e7",
+        greenLight: "#61cea6",
+        grayText: "#818597",
+        lightGray: "#e2e2e2",
+        grayBlue: "#344a6c",
+        deepBlueHead: "#162f56",
+        gray2: "#525a76",
+      },
+    },
   },
   plugins: [],
-}
+};
